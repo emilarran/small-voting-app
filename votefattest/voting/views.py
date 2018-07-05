0921827eb0500d0ca53a2c32e83e6a1e1da5b403
Emil Arran Baleva <arranbaleva@gmail.com>
2018-07-05 21:16:31 +0800

    With data

--- votefattest/voting/views.py
+++ votefattest/voting/views.py
@@ -12,15 +12,23 @@ class ListUsersView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context['profiles'] = UserProfile.objects.all()
         return context
 
 
 class ClapView(View):
 
     def get(self, request, *args, **kwargs):
-        pk = request.GET.get('pk', 0)
+        pk = kwargs['pk']
         profile = get_object_or_404(UserProfile, pk=pk)
         profile.votes = F('votes') + 1
         profile.save()
         profile.refresh_from_db()
-        return HttpResponse(''.format(profile.votes))
+        return HttpResponse('{}'.format(profile.votes))
+
+
+class GetWinning(View):
+
+    def get(self, request, *args, **kwargs):
+        winning = UserProfile.objects.order_by('-votes').first()
+        return HttpResponse('')
